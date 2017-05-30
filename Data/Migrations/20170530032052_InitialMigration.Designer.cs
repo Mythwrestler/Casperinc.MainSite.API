dfc86b5932a3a6fec676642291d099a1b2e8f176
Mythwrester <Mythwrestler@gmail.com>
2017-05-30 07:15:29 -0400

    migrating to VS Mac and minor changes

---
+++ Data/Migrations/20170530032052_InitialMigration.Designer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using CasperInc.MainSiteCore.Data;
+
+namespace MainSiteCore.Data.Migrations
+{
+    [DbContext(typeof(MainSiteCoreDBContext))]
+    [Migration("20170530032052_InitialMigration")]
+    partial class InitialMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("CasperInc.MainSiteCore.Data.Models.NarrativeDataModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("BodyHtml")
+                        .IsRequired();
+
+                    b.Property<DateTime>("CreatedOn");
+
+                    b.Property<string>("Description")
+                        .IsRequired();
+
+                    b.Property<string>("Title")
+                        .IsRequired();
+
+                    b.Property<DateTime>("UpdatedOn");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("NarrativeData");
+                });
+
+            modelBuilder.Entity("CasperInc.MainSiteCore.Data.Models.NarrativeTagDataModel", b =>
+                {
+                    b.Property<Guid>("NarrativeId");
+
+                    b.Property<Guid>("TagId");
+
+                    b.HasKey("NarrativeId", "TagId");
+
+                    b.HasIndex("TagId");
+
+                    b.ToTable("NarrativeTagCrossWalk");
+                });
+
+            modelBuilder.Entity("CasperInc.MainSiteCore.Data.Models.TagDataModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreatedDate");
+
+                    b.Property<string>("KeyWord")
+                        .IsRequired();
+
+                    b.Property<DateTime>("UpdatedDate");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("KeyWord")
+                        .IsUnique();
+
+                    b.ToTable("TagData");
+                });
+
+            modelBuilder.Entity("CasperInc.MainSiteCore.Data.Models.NarrativeTagDataModel", b =>
+                {
+                    b.HasOne("CasperInc.MainSiteCore.Data.Models.NarrativeDataModel", "NarrativeData")
+                        .WithMany("NarrativeTags")
+                        .HasForeignKey("NarrativeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("CasperInc.MainSiteCore.Data.Models.TagDataModel", "TagData")
+                        .WithMany("NarrativeTags")
+                        .HasForeignKey("TagId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Data/Migrations/20170530032052_InitialMigration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainSiteCore.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NarrativeData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    BodyHtml = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    UpdatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NarrativeData", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    KeyWord = table.Column<string>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagData", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NarrativeTagCrossWalk",
+                columns: table => new
+                {
+                    NarrativeId = table.Column<Guid>(nullable: false),
+                    TagId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NarrativeTagCrossWalk", x => new { x.NarrativeId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_NarrativeTagCrossWalk_NarrativeData_NarrativeId",
+                        column: x => x.NarrativeId,
+                        principalTable: "NarrativeData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NarrativeTagCrossWalk_TagData_TagId",
+                        column: x => x.TagId,
+                        principalTable: "TagData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NarrativeTagCrossWalk_TagId",
+                table: "NarrativeTagCrossWalk",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagData_KeyWord",
+                table: "TagData",
+                column: "KeyWord",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NarrativeTagCrossWalk");
+
+            migrationBuilder.DropTable(
+                name: "NarrativeData");
+
+            migrationBuilder.DropTable(
+                name: "TagData");
+        }
+    }
+}
